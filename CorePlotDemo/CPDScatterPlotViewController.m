cc6d74f7403116fbb952169566a694c5e5ff5727
andyzhang <andyzhang26503@gmail.com>
2013-08-05 10:46:38 +0800

    no message

--- CorePlotDemo/CPDScatterPlotViewController.m
+++ CorePlotDemo/CPDScatterPlotViewController.m
@@ -79,11 +79,157 @@
     CPTGraph *graph = self.hostView.hostedGraph;
     CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)graph.defaultPlotSpace;
     
+    CPTScatterPlot *aaplPlot = [[CPTScatterPlot alloc] init];
+    aaplPlot.dataSource = self;
+    aaplPlot.identifier = CPDTickerSymbolAAPL;
+    CPTColor *aaplColor = [CPTColor redColor];
+    [graph addPlot:aaplPlot toPlotSpace:plotSpace];
+    
+    CPTScatterPlot *googPlot = [[CPTScatterPlot alloc] init];
+    googPlot.dataSource = self;
+    googPlot.identifier = CPDTickerSymbolGOOG;
+    CPTColor *googColor = [CPTColor greenColor];
+    [graph addPlot:googPlot toPlotSpace:plotSpace];
+    
+    CPTScatterPlot *msftPlot = [[CPTScatterPlot alloc] init];
+    msftPlot.dataSource = self;
+    msftPlot.identifier = CPDTickerSymbolMSFT;
+    CPTColor *msftColor = [CPTColor blueColor];
+    [graph addPlot:msftPlot toPlotSpace:plotSpace];
+    
+    [plotSpace scaleToFitPlots:[NSArray arrayWithObjects:aaplPlot,googPlot,msftPlot, nil]];
+    CPTMutablePlotRange *xRange = [plotSpace.xRange mutableCopy];
+    [xRange expandRangeByFactor:CPTDecimalFromCGFloat(1.1f)];
+    plotSpace.xRange = xRange;
+    CPTMutablePlotRange *yRange = [plotSpace.yRange mutableCopy];
+    [yRange expandRangeByFactor:CPTDecimalFromCGFloat(1.2f)];
+    plotSpace.yRange = yRange;
+    
+    CPTMutableLineStyle *aaplLineStyle = [aaplPlot.dataLineStyle mutableCopy];
+    aaplLineStyle.lineWidth = 2.5;
+    aaplLineStyle.lineColor = aaplColor;
+    aaplPlot.dataLineStyle = aaplLineStyle;    
+    CPTMutableLineStyle *aaplSymbolLineStyle = [CPTMutableLineStyle lineStyle];
+    aaplSymbolLineStyle.lineColor = aaplColor;
+    CPTPlotSymbol *aaplSymbol = [CPTPlotSymbol ellipsePlotSymbol];
+    aaplSymbol.fill = [CPTFill fillWithColor:aaplColor];
+    aaplSymbol.lineStyle = aaplSymbolLineStyle;
+    aaplSymbol.size = CGSizeMake(6.0f, 6.0f);
+    aaplPlot.plotSymbol = aaplSymbol;
+    
+    CPTMutableLineStyle *googLineStyle = [googPlot.dataLineStyle mutableCopy];
+    googLineStyle.lineWidth = 1.0;
+    googLineStyle.lineColor = googColor;
+    googPlot.dataLineStyle = googLineStyle;
+    CPTMutableLineStyle *googSymbolLineStyle = [CPTMutableLineStyle lineStyle];
+    googSymbolLineStyle.lineColor = googColor;
+    CPTPlotSymbol *googSymbol = [CPTPlotSymbol starPlotSymbol];
+    googSymbol.fill = [CPTFill fillWithColor:googColor];
+    googSymbol.lineStyle = googSymbolLineStyle;
+    googSymbol.size = CGSizeMake(6.0f, 6.0f);
+    googPlot.plotSymbol = googSymbol;
+    
+    CPTMutableLineStyle *msftLineStyle = [msftPlot.dataLineStyle mutableCopy];
+    msftLineStyle.lineWidth = 2.0;
+    msftLineStyle.lineColor = msftColor;
+    msftPlot.dataLineStyle = msftLineStyle;
+    CPTMutableLineStyle *msftSymbolLineStyle = [CPTMutableLineStyle lineStyle];
+    msftSymbolLineStyle.lineColor = msftColor;
+    CPTPlotSymbol *msftSymbol = [CPTPlotSymbol diamondPlotSymbol];
+    msftSymbol.fill = [CPTFill fillWithColor:msftColor];
+    msftSymbol.lineStyle = msftSymbolLineStyle;
+    msftSymbol.size = CGSizeMake(6.0f, 6.0f);
+    msftPlot.plotSymbol = msftSymbol;
 }
 
 - (void)configureAxes
 {
-    
+    // 1 - Create styles
+    CPTMutableTextStyle *axisTitleStyle = [CPTMutableTextStyle textStyle];
+    axisTitleStyle.color = [CPTColor whiteColor];
+    axisTitleStyle.fontName = @"Helvetica-Bold";
+    axisTitleStyle.fontSize = 12.0f;
+    CPTMutableLineStyle *axisLineStyle = [CPTMutableLineStyle lineStyle];
+    axisLineStyle.lineWidth = 2.0f;
+    axisLineStyle.lineColor = [CPTColor whiteColor];
+    CPTMutableTextStyle *axisTextStyle = [[CPTMutableTextStyle alloc] init];
+    axisTextStyle.color = [CPTColor whiteColor];
+    axisTextStyle.fontName = @"Helvetica-Bold";
+    axisTextStyle.fontSize = 11.0f;
+    CPTMutableLineStyle *tickLineStyle = [CPTMutableLineStyle lineStyle];
+    tickLineStyle.lineColor = [CPTColor whiteColor];
+    tickLineStyle.lineWidth = 2.0f;
+    CPTMutableLineStyle *gridLineStyle = [CPTMutableLineStyle lineStyle];
+    tickLineStyle.lineColor = [CPTColor blackColor];
+    tickLineStyle.lineWidth = 1.0f;
+    // 2 - Get axis set
+    CPTXYAxisSet *axisSet = (CPTXYAxisSet *) self.hostView.hostedGraph.axisSet;
+    // 3 - Configure x-axis
+    CPTAxis *x = axisSet.xAxis;
+    x.title = @"Day of Month";
+    x.titleTextStyle = axisTitleStyle;
+    x.titleOffset = 15.0f;
+    x.axisLineStyle = axisLineStyle;
+    x.labelingPolicy = CPTAxisLabelingPolicyNone;
+    x.labelTextStyle = axisTextStyle;
+    x.majorTickLineStyle = axisLineStyle;
+    x.majorTickLength = 4.0f;
+    x.tickDirection = CPTSignNegative;
+    CGFloat dateCount = [[[CPDStockPriceStore sharedInstance] datesInMonth] count];
+    NSMutableSet *xLabels = [NSMutableSet setWithCapacity:dateCount];
+    NSMutableSet *xLocations = [NSMutableSet setWithCapacity:dateCount];
+    NSInteger i = 0;
+    for (NSString *date in [[CPDStockPriceStore sharedInstance] datesInMonth]) {
+        CPTAxisLabel *label = [[CPTAxisLabel alloc] initWithText:date  textStyle:x.labelTextStyle];
+        CGFloat location = i++;
+        label.tickLocation = CPTDecimalFromCGFloat(location);
+        label.offset = x.majorTickLength;
+        if (label) {
+            [xLabels addObject:label];
+            [xLocations addObject:[NSNumber numberWithFloat:location]];
+        }
+    }
+    x.axisLabels = xLabels;
+    x.majorTickLocations = xLocations;
+    // 4 - Configure y-axis
+    CPTAxis *y = axisSet.yAxis;
+    y.title = @"Price";
+    y.titleTextStyle = axisTitleStyle;
+    y.titleOffset = -40.0f;
+    y.axisLineStyle = axisLineStyle;
+    y.majorGridLineStyle = gridLineStyle;
+    y.labelingPolicy = CPTAxisLabelingPolicyNone;
+    y.labelTextStyle = axisTextStyle;
+    y.labelOffset = 16.0f;
+    y.majorTickLineStyle = axisLineStyle;
+    y.majorTickLength = 4.0f;
+    y.minorTickLength = 2.0f;
+    y.tickDirection = CPTSignPositive;
+    NSInteger majorIncrement = 100;
+    NSInteger minorIncrement = 50;
+    CGFloat yMax = 700.0f;  // should determine dynamically based on max price
+    NSMutableSet *yLabels = [NSMutableSet set];
+    NSMutableSet *yMajorLocations = [NSMutableSet set];
+    NSMutableSet *yMinorLocations = [NSMutableSet set];
+    for (NSInteger j = minorIncrement; j <= yMax; j += minorIncrement) {
+        NSUInteger mod = j % majorIncrement;
+        if (mod == 0) {
+            CPTAxisLabel *label = [[CPTAxisLabel alloc] initWithText:[NSString stringWithFormat:@"%i", j] textStyle:y.labelTextStyle];
+            NSDecimal location = CPTDecimalFromInteger(j);
+            label.tickLocation = location;
+            label.offset = -y.majorTickLength - y.labelOffset;
+            if (label) {
+                [yLabels addObject:label];
+            }
+            [yMajorLocations addObject:[NSDecimalNumber decimalNumberWithDecimal:location]];
+        } else {
+            [yMinorLocations addObject:[NSDecimalNumber decimalNumberWithDecimal:CPTDecimalFromInteger(j)]];
+        }
+    }
+    y.axisLabels = yLabels;    
+    y.majorTickLocations = yMajorLocations;
+    y.minorTickLocations = yMinorLocations;
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -95,11 +241,30 @@
 #pragma mark - CPTPlotDataSource methods
 - (NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot
 {
-    return 0;
+    return [[[CPDStockPriceStore sharedInstance] datesInMonth] count];
 }
 
 - (NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)idx
 {
+    NSInteger valueCount = [[[CPDStockPriceStore sharedInstance] datesInMonth] count];
+    switch (fieldEnum) {
+        case CPTScatterPlotFieldX:
+            if (idx <valueCount) {
+                return [NSNumber numberWithUnsignedInteger:idx];
+            }
+            break;
+            
+        case CPTScatterPlotFieldY:
+            if ([plot.identifier isEqual:CPDTickerSymbolAAPL]) {
+                return [[[CPDStockPriceStore sharedInstance] monthlyPrices:CPDTickerSymbolAAPL] objectAtIndex:idx];
+            }else if ([plot.identifier isEqual:CPDTickerSymbolGOOG] == YES) {
+                return [[[CPDStockPriceStore sharedInstance] monthlyPrices:CPDTickerSymbolGOOG] objectAtIndex:idx];
+            } else if ([plot.identifier isEqual:CPDTickerSymbolMSFT] == YES) {
+                return [[[CPDStockPriceStore sharedInstance] monthlyPrices:CPDTickerSymbolMSFT] objectAtIndex:idx];
+            }
+            break;
+            
+    }
     return [NSDecimalNumber zero];
 }
 @end
